f5731a35a48de51163ac8e20058fa5e6cf1e342e
sullins2 <sullins2@uic.edu>
2023-07-07 18:58:39 -0500

    betm=8 tests

--- game-master/experiment.py
+++ game-master/experiment.py
@@ -31,7 +31,7 @@ algo="cfr"
 # algo="lazycfr"
 # algo="komwu"
 # algo="lazyflbr"
-# algo="lazycfr_nocomments"  # This is KOMWU
+algo="lazycfr_nocomments"  # This is KOMWU
 
 # Type = "regretmatching"
 # Type = "regretmatchingplus"
@@ -46,7 +46,7 @@ dcfr_params = [1.5, 0.0, 2.0]
 if betm>7:
 	game = Game(path=savepath+".npz")
 else:
-	betm = 3
+	betm = 8
 	game = Game( bidmaximum =betm) #path=savepath+".npz")#bidmaximum=betmpath=
 
 # This is for Kuhn - doesn't have betm..
