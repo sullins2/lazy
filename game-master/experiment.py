5a6742605cd5f1fa59ab2c2d5774fdfffc4984bd
sullins2 <35474542+sullins2@users.noreply.github.com>
2023-08-02 10:00:50 -0500

    Update experiment.py

--- game-master/experiment.py
+++ game-master/experiment.py
@@ -365,11 +365,11 @@ def run(game, path="result", Type="regretmatching", solvername = "cfr"):
 betm = 7
 # TODO LEDUC 4 IS RUNNING
 game = Game( bidmaximum =betm)
-params["thres"] = 0.005
+params["thres"] = 0.008
 params["eta"] = 20.0 #1.0
 params["final_exploit"] = 1e-12
-params["b_count"] = [20] # Set to negative to disable
-params["b_count_count_at"] = [10]
+params["b_count"] = [10] # Set to negative to disable
+params["b_count_count_at"] = [5]
 print(params)
 res = run(game, Type=Type, solvername=algo)
 its0 = res[5]
@@ -377,7 +377,7 @@ plots0 = res[6]
 
 game = Game( bidmaximum =betm) #path=savepath+".npz")#bidmaximum=betmpath=
 # game = KuhnGame( bidmaximum =betm)
-params["thres"] = 0.006
+params["thres"] = 0.008
 params["eta"] = 20.0 #1.0
 params["final_exploit"] = 1e-12
 params["b_count"] = [20] # Set to negative to disable
