c4a25bca1f1ba38c63e38c5c5896e8f7dd1c35f0
sullins2 <35474542+sullins2@users.noreply.github.com>
2023-08-03 10:58:23 -0500

    Update experiment.py

--- game-master/experiment.py
+++ game-master/experiment.py
@@ -363,7 +363,7 @@ def run(game, path="result", Type="regretmatching", solvername = "cfr"):
 # res = run(game, Type=Type, solvername=algo)
 
 # game = KuhnGame( bidmaximum =betm)
-betm = 5
+betm = 3
 # TODO LEDUC 4 IS RUNNING
 game = Game( bidmaximum =betm)
 params["thres"] = 0.2
